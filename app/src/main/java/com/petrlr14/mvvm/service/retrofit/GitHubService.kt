7af3daa63a1218e06b32120a15463b7a6f69545c
Marlpzg <00046317@uca.edu.sv>
2019-05-31 10:47:04 -0600

    Retrofit

---
+++ app/src/main/java/com/petrlr14/mvvm/service/retrofit/GitHubService.kt
@@ -0,0 +1,38 @@
+package com.petrlr14.mvvm.service.retrofit
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+const val GITHUB_BASE_URL = "https://api.github.com/"
+
+interface GitHubService {
+
+    @GET("users/{user}/repos")
+    fun getRepos(@Path("user") user: String): Deferred<Response<List<GitHubRepo>>>
+
+    companion object{
+        var INSTANCE: GitHubService? = null
+
+        fun getGitHubService(): GitHubService{
+            if (INSTANCE != null) {
+                return INSTANCE!!
+            }
+            INSTANCE = Retrofit
+                .Builder()
+                .baseUrl(GITHUB_BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+                .create(GitHubService::class.java)
+            return INSTANCE!!
+        }
+    }
+
+}
